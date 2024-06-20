7d434bb1e7f48f97964a4a1336401020eaf6b42a
Pryscila Martins <pryscilamartins@hotmail.com>
2024-06-19 23:44:40 -0300

    Implementação das telas: Cadastro, login, Cadastro de crianças.

---
+++ src/NomeDoProjeto/app/(tabs)/cadastro.tsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Pressable, Alert } from 'react-native';
+import { NavigationProp } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import config from './config';
+
+type RootStackParamList = {
+  telaLogin: undefined;
+  index: undefined;
+  selecionaAluno: { userId: number };
+  pessoasAutorizadas: undefined;
+  telaCadastro: undefined;
+};
+
+export default function telaCadastro() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [responsavelPeloAluno, setResponsavelPeloAluno] = useState('');
+  const navigation = useNavigation<NavigationProp<RootStackParamList, 'selecionaAluno'>>();
+
+  const handleCadastro = () => {
+    fetch(config.URL+'/Cadastro', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ nome, senha, responsavelPeloAluno, email })
+    })
+    .then(response => {
+      if (response.status === 200) {
+        Alert.alert('Sucesso', 'Cadastro realizado com sucesso');
+        navigation.navigate('telaLogin');
+      } else {
+        throw new Error('Falha no cadastro. Verifique os dados e tente novamente.');
+      }
+    })
+    .catch(error => {
+      if (error instanceof Error) {
+        Alert.alert('Erro', error.message);
+      } else {
+        Alert.alert('Erro', 'Ocorreu um erro.');
+      }
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Cadastro</Text>
+      <Text style={styles.label}>Nome</Text>
+      <TextInput
+        style={styles.input}
+        value={nome}
+        onChangeText={setNome}
+      />
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+      />
+      <Text style={styles.label}>Senha</Text>
+      <TextInput
+        style={styles.input}
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry={true}
+      />
+      <Text style={styles.label}>Responsável pelo Aluno</Text>
+      <TextInput
+        style={styles.input}
+        value={responsavelPeloAluno}
+        onChangeText={setResponsavelPeloAluno}
+      />
+      <Pressable style={styles.button} onPress={handleCadastro}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D9D9D9',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    color: 'black',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  label: {
+    color: 'black',
+    marginBottom: 5,
+    fontSize: 16,
+  },
+  input: {
+    backgroundColor: '#7BC59D',
+    width: '100%',
+    height: 40,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#7BC59D',
+    width: '100%',
+    height: 40,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
---
+++ src/NomeDoProjeto/app/(tabs)/listarCriancas.tsx
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, TextInput, Pressable, Alert, FlatList } from 'react-native';
+import { NavigationProp } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import config from './config';
+
+type RootStackParamList = {
+  telaLogin: undefined;
+  index: undefined;
+  selecionaAluno: { userId: number };
+  pessoasAutorizadas: undefined;
+  telaCadastro: undefined;
+};
+
+export default function listarCriancas() {
+    useEffect(() => {
+        carregarAlunos()
+         },[])
+    const [alunos, setAlunos] = useState([]);
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+  const carregarAlunos = () => {
+    fetch(config.URL+'/Matricula', {
+      method: 'GET',
+    })
+      .then(response => {
+        if (response.status === 200) {
+            //setAlunos(response.json())
+            return response.json();
+        } else {
+          throw new Error('Usuário ou senha inválidos.');
+        }
+      })
+      .then(data => {
+        setAlunos(data);
+        // Navegar para a tela de pessoas autorizadas
+        //navigation.navigate('opcoesResponsavel');
+      })
+      .catch(error => {
+        if (error instanceof Error) {
+          Alert.alert('Erro', error.message);
+        } else {
+          Alert.alert('Erro', 'Ocorreu um erro.');
+        }
+      });
+  };
+
+  const handleNavigateToCadastro = () => {
+    navigation.navigate('telaCadastro');
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemText}>Nome: {item.nomeAluno}</Text>
+      <Text style={styles.itemText}>Endereço: {item.endereco}</Text>
+      <Text style={styles.itemText}>Responsável: {item.responsavel}</Text>
+      <Text style={styles.itemText}>Professor: {item.professor}</Text>
+      <Text style={styles.itemText}>Matrícula: {item.matricula}</Text>
+      <Text style={styles.itemText}>Idade: {item.idadeAluno}</Text>
+      <Text style={styles.itemText}>Série: {item.serieAluno}</Text>
+      <Text style={styles.itemText}>Sala: {item.salaAluno}</Text>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={alunos}
+        keyExtractor={item => item.id.toString()}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D9D9D9',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    color: 'black',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  label: {
+    color: 'black',
+    marginBottom: 5,
+    fontSize: 16,
+  },
+  input: {
+    backgroundColor: '#7BC59D',
+    width: '100%',
+    height: 40,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#7BC59D',
+    width: '100%',
+    height: 40,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  forgotPassword: {
+    color: 'black',
+    fontSize: 15,
+    marginTop: 20,
+  },
+  itemContainer: {
+    backgroundColor: '#fff',
+    padding: 20,
+    marginVertical: 8,
+    borderRadius: 5,
+    width: '100%',
+  },
+  itemText: {
+    fontSize: 16,
+    color: 'black',
+  },
+});
